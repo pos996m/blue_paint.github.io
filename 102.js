e6b224d94cccecb848963eb0985c2c174a2a7f79
pos996m <pos996m@gmail.com>
2021-08-18 12:43:59 +0800

    add: 高度與寬度隔開輸入 add: 輸入參數可以使用enter來輸入 add: 設定輸入最高值與只能輸入數字 change: CSS 排版 change: html的標籤放置

--- 102.js
+++ 102.js
@@ -1,34 +1,49 @@
-var xx = parseInt(prompt("輸入繪製高度:"));
-while (isNaN(xx)) {
-    xx = parseInt(prompt("請輸入數字\n輸入寬:"));
-}
+var xx = 20;
+var yy = 20;
 
-var yy = parseInt(prompt("輸入繪製寬度:"));
-while (isNaN(yy)) {
-    yy = parseInt(prompt("請輸入數字\n輸入高:"));
-}
+// 初始建立
+var change_sizepx = "10px";
+build_td();
 
-// 地圖建立過程c
-var td_all = "<td class='td_all' id='td_1' onclick='ontest(event)';></td>";
-// var tr_all;
-for (let i = 2; i < (xx * yy) + 1; i++) {
-    td_all += `<td class='td_all' id='td_${i}' onclick='ontest(event)';></td>`;
-    if (i == (xx * yy)) {
-        td_all += "</tr>"
-    } else if ((i % yy) == 0) {
-        td_all += "</tr><tr>"
+
+function build_td() {
+    // 地圖建立過程c
+    var td_all = "<td class='td_all' id='td_1' onclick='ontest(event)' touchstart='ontest(event)';></td>";
+    // var tr_all;
+    for (let i = 2; i < (xx * yy) + 1; i++) {
+        td_all += `<td class='td_all' id='td_${i}' onclick='ontest(event)' touchstart='ontest(event)';></td>`;
+        if (i == (xx * yy)) {
+            td_all += "</tr>"
+        } else if ((i % yy) == 0) {
+            td_all += "</tr><tr>"
+        }
+    }
+    td_all = "<tr>" + td_all
+
+    // 建立標籤
+    var table_all = document.getElementById("table_all");
+    table_all.innerHTML = td_all;
+
+    // 建立td_px
+    for (let i = 1; i < (xx * yy); i++) {
+        document.getElementById(`td_${i}`).style.width = change_sizepx;
+        document.getElementById(`td_${i}`).style.height = change_sizepx;
     }
 }
-td_all = "<tr>" + td_all
 
-// 建立標籤
-var table_all = document.getElementById("table_all");
-table_all.innerHTML = td_all;
+function change_td(){
+    xx = document.getElementById("change_H").value;
+    yy = document.getElementById("change_W").value;
+    build_td();
+}
 
-// 建立td_px
-for (let i = 1; i < (xx * yy); i++) {
-    document.getElementById(`td_${i}`).style.width = "10px";
-    document.getElementById(`td_${i}`).style.height = "10px";
+// 換格子尺寸
+function change_size() {
+    change_sizepx = document.getElementById("change_size").value;
+    for (let i = 1; i < (xx * yy); i++) {
+        document.getElementById(`td_${i}`).style.width = change_sizepx;
+        document.getElementById(`td_${i}`).style.height = change_sizepx;
+    }
 }
 
 var del_C = true;
@@ -62,10 +77,10 @@ var del_color = false;
 // 判斷按住ctrl
 function movekeydown(event) {
     // console.log(event);
-    if(event.key == "Control"){
+    if (event.key == "Control") {
         key_colour = document.getElementById("color_sup").value;
     }
-    if(event.key == "Shift"){
+    if (event.key == "Shift") {
         del_color = true;
     }
 }
@@ -77,19 +92,13 @@ function movekeyup(event) {
 }
 
 // 選擇顏色事件
-function change_mycolor(){
+function change_mycolor() {
     key_colour = document.getElementById("color_main").value;
 }
 
 
-// 換格子尺寸
-function change_size(){
-    var x = document.getElementById("change_size").value;
-    for (let i = 1; i < (xx * yy); i++) {
-        document.getElementById(`td_${i}`).style.width = x;
-        document.getElementById(`td_${i}`).style.height = x;
-    }
-}
+
+
 
 
 // 寬高功能拉出來獨立
