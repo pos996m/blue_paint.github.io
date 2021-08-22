9361b818f686aebe73f8f28d2b796513462f30cf
pos996m <pos996m@gmail.com>
2021-08-23 02:04:21 +0800

    add: ctrl +　z 回上一步驟功能

--- 102.js
+++ 102.js
@@ -146,11 +146,20 @@ function movekeydown(event) {
     if (event.key == "Alt") {
         alt_color = true;
     }
-    if (event.key == "x") {
+    if (event.key == "x" || event.key == "X") {
         var x = document.getElementById("color_main").value;
         var y = document.getElementById("color_sup").value;
         document.getElementById("color_main").value = y;
         document.getElementById("color_sup").value = x;
+    }
+
+    // ctrl + z，回到上一步
+    if (event.ctrlKey && event.key == "z" || event.key == "Z") {
+        // console.log(recording);
+        if (recording.length != 1) {
+            re_map(recording[recording.length - 2]);
+            recording.pop();
+        }
     }
 }
 
@@ -179,55 +188,83 @@ function clearPicture() {
 }
 
 
-// 輸出表格編號
-// 記錄哪幾格表格被使用到 如同將圖片輸出
-function outputNumber() {
-    let temp_id = [];
-    let temp_color_arr = [];
+// 表格編號
+// 作為記錄檔使用
+function outputNumber(event) {
+
+    if (event.path[0].id.indexOf("td") != -1 || event.path[0].id.indexOf("table") != -1) {
+
+
+        let temp_id = [];
+        let temp_color_arr = [];
+
+        // 使用迴圈逐一比對
+        for (let index = 1; index <= (xx * yy); index++) {
+            // 如果該 td 顏色 非空 則是有顏色 應當被記錄
+            if (document.getElementById(`td_${index}`).style.backgroundColor != "") {
+                // 加入陣列中
+                let x_color = document.getElementById(`td_${index}`).style.backgroundColor;
+                // let temp_color = F_get_color16(x_color);
+                temp_id.push(`${index}@${x_color}`);
+                if (temp_color_arr.indexOf(x_color) == -1) {
+                    temp_color_arr.push(x_color);
+                }
+            }
+        }
+
+        // console.log(temp_id)
+        // console.log(temp_color_arr)
+        var over_color = [];
+
+        // 選取相應的顏色配對編號
+        for (let i = 0; i < temp_color_arr.length; i++) {
+            var idc = `=${F_get_color16(temp_color_arr[i])},`;
+            for (let k = 0; k < temp_id.length; k++) {
+                // console.log(temp_id[k].substr(temp_id[k].indexOf("@")+1, temp_id[k].length - temp_id[k].indexOf("@") - 1));
+                if (temp_id[k].substr(temp_id[k].indexOf("@") + 1, temp_id[k].length - temp_id[k].indexOf("@") - 1) == temp_color_arr[i]) {
+
+                    idc += `${temp_id[k].substr(0, temp_id[k].indexOf("@"))},`;
+
+                }
+            }
+            idc = idc.substr(0, idc.length - 1);
+            over_color.push(idc);
+        }
+
+
+        // 刪除過多紀錄
+        if (recording.length > 10) {
+            recording.shift();
+        }
+
+        // 加入進變數(用來記錄上一步驟)
+        recording.push(`=${xx}=${yy}=${change_sizepx}`);
+        for (let p = 0; p < over_color.length; p++) {
+            recording[recording.length - 1] += `${over_color[p]}`;
+        }
+
+    }
+    // // 加入大小編號與px編號
+    // document.getElementById('outNB').value += `=${xx}=${yy}=${change_sizepx}`
+
+    // // 輸出顏色與編號
+    // for (let p = 0; p < over_color.length; p++) {
+    //     document.getElementById('outNB').value += `${over_color[p]}`;
+    // }
+}
+
+var recording = [];
+recording.push(`=${xx}=${yy}=${change_sizepx}`)
+
+// 點按輸出記錄檔
+function clickNumber() {
     // 將顯示區域 初始化
     document.getElementById('outNB').value = "";
-    // 使用迴圈逐一比對
-    for (let index = 1; index <= (xx * yy); index++) {
-        // 如果該 td 顏色 非空 則是有顏色 應當被記錄
-        if (document.getElementById(`td_${index}`).style.backgroundColor != "") {
-            // 加入陣列中
-            let x_color = document.getElementById(`td_${index}`).style.backgroundColor;
-            // let temp_color = F_get_color16(x_color);
-            temp_id.push(`${index}@${x_color}`);
-            if (temp_color_arr.indexOf(x_color) == -1) {
-                temp_color_arr.push(x_color);
-            }
-        }
-    }
 
-    // console.log(temp_id)
-    // console.log(temp_color_arr)
-    var over_color = [];
-
-    // 選取相應的顏色配對編號
-    for (let i = 0; i < temp_color_arr.length; i++) {
-        var idc = `=${F_get_color16(temp_color_arr[i])},`;
-        for (let k = 0; k < temp_id.length; k++) {
-            // console.log(temp_id[k].substr(temp_id[k].indexOf("@")+1, temp_id[k].length - temp_id[k].indexOf("@") - 1));
-            if (temp_id[k].substr(temp_id[k].indexOf("@") + 1, temp_id[k].length - temp_id[k].indexOf("@") - 1) == temp_color_arr[i]) {
-
-                idc += `${temp_id[k].substr(0, temp_id[k].indexOf("@"))},`;
-
-            }
-        }
-        idc = idc.substr(0, idc.length - 1);
-        over_color.push(idc);
-    }
-
-    // 加入大小編號與px編號
-    document.getElementById('outNB').value += `=${xx}=${yy}=${change_sizepx}`
-
-    // 輸出顏色與編號
-    for (let p = 0; p < over_color.length; p++) {
-        document.getElementById('outNB').value += `${over_color[p]}`;
-    }
-
+    document.getElementById('outNB').value = recording[recording.length - 1];
 }
+
+
 
 // 讀取記錄檔
 function print_map() {
@@ -236,10 +273,20 @@ function print_map() {
     // 去除換行
     textArea_V = textArea_V.replace(/[\r\n]/g, "");
 
+    // 轉換記錄檔
+    re_map(textArea_V);
+
+
+    document.getElementById('outNB').value = "讀取成功";
+}
+
+// 記錄檔轉換
+function re_map(textArea_V) {
+
     // 第一次陣列轉換
     let inptdNumber = textArea_V.split('=');
     inptdNumber.shift();
-    console.log(inptdNumber);
+    // console.log(inptdNumber);
 
     // 新建陣列用來存放二維陣列
     let arrin = [];
@@ -248,7 +295,7 @@ function print_map() {
     for (let i = 0; i < inptdNumber.length; i++) {
         arrin.push(inptdNumber[i].split(','));
     }
-    console.log(arrin)
+    // console.log(arrin)
     // console.log(arrin[0][0])
 
     // 讀取地圖的值
@@ -267,8 +314,8 @@ function print_map() {
         }
     }
 
-    document.getElementById('outNB').value = "讀取成功";
 }
+
 
 // 一鍵複製
 function CopyTextToClipboard(id) {
